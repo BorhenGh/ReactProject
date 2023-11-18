bcdfab0a59d88c7bce872a933f890dff584fca35
borhen.ghrairi <borhenghrairi7@gmail.com>
2023-11-18 21:09:46 +0100

    First Step In Project

--- src/BooksQ.js
+++ src/BooksQ.js
@@ -1,64 +1,113 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+import axios from 'axios';
 import './BooksQ.css';
+
+const initialState = {
+  reference: '',
+  keyword: '',
+  data: null,
+  loading: false,
+  error: null,
+};
+
+const actions = {
+  SET_REFERENCE: 'SET_REFERENCE',
+  SET_KEYWORD: 'SET_KEYWORD',
+  FETCH_START: 'FETCH_START',
+  FETCH_SUCCESS: 'FETCH_SUCCESS',
+  FETCH_ERROR: 'FETCH_ERROR',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.SET_REFERENCE:
+      return { ...state, reference: action.payload };
+    case actions.SET_KEYWORD:
+      return { ...state, keyword: action.payload };
+    case actions.FETCH_START:
+      return { ...state, loading: true, error: null };
+    case actions.FETCH_SUCCESS:
+      return { ...state, data: action.payload, loading: false };
+    case actions.FETCH_ERROR:
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
 const BooksQ = () => {
-  const [reference, setReference] = useState('');
-  const [keyword, setKeyword] = useState('');
-  const [data, setData] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchVerse = async () => {
+  const handleSearch = async () => {
+    dispatch({ type: actions.FETCH_START });
+
     try {
-      const response = await fetch(`http://api.alquran.cloud/v1/ayah/${reference}/en-saheeh`);
-      const jsonData = await response.json();
-      setData([jsonData.data]);
+      let response;
+      if (state.reference) {
+        response = await axios.get(`http://api.alquran.cloud/v1/ayah/${state.reference}/en-saheeh`);
+      } else if (state.keyword) {
+        response = await axios.get(`http://api.alquran.cloud/v1/search/${state.keyword}/all/en`);
+      }
+
+      dispatch({ type: actions.FETCH_SUCCESS, payload: response.data.data.matches || [response.data.data] });
     } catch (error) {
+      dispatch({ type: actions.FETCH_ERROR, payload: 'Une erreur s\'est produite' });
       console.error(error);
     }
   };
 
-  const fetchSearchResults = async () => {
-    try {
-      const response = await fetch(`http://api.alquran.cloud/v1/search/${keyword}/all/en`);
-      const jsonData = await response.json();
-      setData(jsonData.data.matches);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSearch = () => {
-    if (reference) {
-      fetchVerse();
-    } else if (keyword) {
-      fetchSearchResults();
+  // Ajout d'une classe pour déclencher l'animation
+  const fadeInResults = () => {
+    const resultContainer = document.querySelector('.result-container');
+    if (resultContainer) {
+      resultContainer.classList.add('fade-in');
     }
   };
 
   return (
     <div className="booksq-container">
       <div>
-        <input type="text" value={reference} onChange={(e) => setReference(e.target.value)} placeholder="Enter verse reference" />
-        <button className="button button-primary" onClick={handleSearch}>Search reverse</button>   </div> <br/><br/><br/>
-       <di> <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Enter keyword" />
-        <button onClick={handleSearch}>Search Keyword</button></di>
-      <div>
-       
+        <input
+          type="text"
+          value={state.reference}
+          onChange={(e) => dispatch({ type: actions.SET_REFERENCE, payload: e.target.value })}
+          placeholder="Enter verse reference"
+        />
+        <button className="button button-primary" onClick={() => { handleSearch(); fadeInResults(); }}>
+          Search reverse
+        </button>
       </div>
-
-      {data && (
-        <div className="result-container">
-          <p className="result-title">Search Results</p>
-          {data.map((item, index) => (
-            <div key={index}>
-              <p className="result-number">Number: {item?.number}</p>
-              <p className="result-text">Text: {item?.text}</p>
-              <p className="result-edition">Edition: {item?.edition?.name}</p>
-              <p className="result-surah-number">Surah Number: {item?.surah?.number}</p>
-              <p className="result-surah-name">Surah Name: {item?.surah?.name}</p>
-              <hr />
-            </div>
-          ))}
-        </div>
-      )}
+      <br />
+      <br />
+      <br />
+      <div>
+        <input
+          type="text"
+          value={state.keyword}
+          onChange={(e) => dispatch({ type: actions.SET_KEYWORD, payload: e.target.value })}
+          placeholder="Enter keyword"
+        />
+        <button onClick={() => { handleSearch(); fadeInResults(); }}>
+          Search Keyword
+        </button>
+      </div>
+      <div className={`result-container ${state.data ? 'fade-in' : ''}`}>
+        {state.data && (
+          <>
+            <p className="result-title">Search Results</p>
+            {state.data.map((item, index) => (
+              <div key={index}>
+                <p className="result-number">Number: {item?.number}</p>
+                <p className="result-text">Text: {item?.text}</p>
+                <p className="result-edition">Edition: {item?.edition?.name}</p>
+                <p className="result-surah-number">Surah Number: {item?.surah?.number}</p>
+                <p className="result-surah-name">Surah Name: {item?.surah?.name}</p>
+                <hr />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Home() {
+  const [showQuranAlert, setShowQuranAlert] = useState(false);
+  const [showSurahAlert, setShowSurahAlert] = useState(false);
+  const navigate = useNavigate();
+
+  const handleQuranButtonClick = async () => {
+    setShowQuranAlert(true);
+    try {
+      // Add specific Quran logic here if needed
+      // For example, make an API request using axios
+      const response = await axios.get('https://api.example.com/quran');
+      console.log(response.data); // Log the response data
+    } catch (error) {
+      console.error('Error fetching Quran data', error);
+    } finally {
+      setTimeout(() => {
+        setShowQuranAlert(false);
+        navigate('/boo');
+      }, 3000);
+    }
+  };
+
+  const handleSurahButtonClick = async () => {
+    setShowSurahAlert(true);
+    try {
+      // Add specific Surah logic here if needed
+      // For example, make an API request using axios
+      const response = await axios.get('https://api.example.com/surah');
+      console.log(response.data); // Log the response data
+    } catch (error) {
+      console.error('Error fetching Surah data', error);
+    } finally {
+      setTimeout(() => {
+        setShowSurahAlert(false);
+        navigate('/Surah');
+      }, 3000);
+    }
+  };
+
   return (
     <div className="home-container">
       <div className="background-image"></div>
@@ -10,11 +49,27 @@ function Home() {
         <h1 className="home-title">Bienvenue dans notre bibliothèque</h1>
         <p className="home-description">Découvrez notre vaste collection de livres</p>
         <div className="button-container">
-          <Link to="/boo" className="home-button">Quran</Link>
-          <Link to="/Surah" className="home-button">Surah</Link>
-       
+          <button className="home-button" onClick={handleQuranButtonClick}>
+            Quran
+          </button>
+          <button className="home-button" onClick={handleSurahButtonClick}>
+            Surah
+          </button>
         </div>
       </div>
+
+      {/* Custom Alerts */}
+      {showQuranAlert && (
+        <div className="custom-alert">
+          <p>Contenu de l'alerte pour Quran</p>
+        </div>
+      )}
+
+      {showSurahAlert && (
+        <div className="custom-alert">
+          <p>Contenu de l'alerte pour Surah</p>
+        </div>
+      )}
     </div>
   );
 }
